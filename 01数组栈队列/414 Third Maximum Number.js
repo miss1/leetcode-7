9ccfa24622e47744921dbe6d665a2ade32de8182
Lynn Yang <yangll2525@163.com>
2022-03-28 19:32:10 +0800

    first commit

---
+++ 01数组栈队列/414 Third Maximum Number.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * time: O(n)
+ * space: O(1)
+ */
+var thirdMax = function(nums) {
+	let first = nums[0], second = -Infinity, third = -Infinity;
+	for (let i = 1; i < nums.length; i++) {
+		if (nums[i] > first) {
+			third = second;
+			second = first;
+			first = nums[i];
+		} else if (nums[i] > second && nums[i] !== first) {
+			third = second;
+			second = nums[i];
+		} else if (nums[i] < second && nums[i] > third) {
+			third = nums[i];
+		}
+	}
+	return third === -Infinity ? first : third;
+};
