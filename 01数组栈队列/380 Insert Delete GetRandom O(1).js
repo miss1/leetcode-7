39f505b42ad76426d2f7ee5a545fc9654e757ca6
Lynn Yang <yangll2525@163.com>
2022-03-29 22:49:32 +0800

    380

---
+++ 01数组栈队列/380 Insert Delete GetRandom O(1).js
@@ -0,0 +1,59 @@
+
+var RandomizedSet = function() {
+  this.m = new Map();
+  this.arr = [];
+};
+
+/**
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.insert = function(val) {
+  if(this.m.get(val) === undefined) {
+    this.arr.push(val);
+    this.m.set(val, this.arr.length - 1);
+    return true;
+  }
+  return false;
+};
+
+/**
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.remove = function(val) {
+  if(this.m.get(val) !== undefined) {
+    let index = this.m.get(val);
+    let last = this.arr[this.arr.length - 1];
+    this.arr[index] = last;
+    this.m.set(last, index);
+    this.arr.pop();
+    this.m.delete(val);
+    return true;
+  }
+  return false;
+};
+
+/**
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function() {
+  let index = Math.floor(Math.random() * this.arr.length);
+  return this.arr[index];
+};
+
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
+
+/**
+ * time: O(1)
+ * space: O(n)
+ * 利用数组记录值，用map记录值对应的下标，删除的时候将数组最后一位放到要删除的值的下标位置，再将最后一位pop出去
+ */
+
