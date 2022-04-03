ebd0575601cba62939183ecc54bd245a19f11e8c
Lynn Yang <yangll2525@163.com>
2022-04-03 14:33:05 +0800

    1381

---
+++ 01数组栈队列/1381 Design a Stack With Increment Operation.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number} maxSize
+ */
+var CustomStack = function(maxSize) {
+  this.arr = [];
+  this.maxSize = maxSize;
+};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+CustomStack.prototype.push = function(x) {
+  if (this.arr.length < this.maxSize) this.arr.push(x);
+};
+
+/**
+ * @return {number}
+ */
+CustomStack.prototype.pop = function() {
+  if (this.arr.length === 0) return -1;
+  else return this.arr.pop();
+};
+
+/**
+ * @param {number} k
+ * @param {number} val
+ * @return {void}
+ */
+CustomStack.prototype.increment = function(k, val) {
+  for (let i = 0; i < k; i++) {
+    if (i >= this.arr.length) break;
+    this.arr[i] += val;
+  }
+};
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * var obj = new CustomStack(maxSize)
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * obj.increment(k,val)
+ */
+
+/**
+ * time: O(n)
+ * space: O(n)
+*/
