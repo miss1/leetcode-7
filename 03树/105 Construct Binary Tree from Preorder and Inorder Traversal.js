5a65e1e7dec8c3aeddd963edf00293516d7a0bc7
Lynn Yang <yangll2525@163.com>
2022-04-10 15:51:33 +0800

    105

---
+++ 03树/105 Construct Binary Tree from Preorder and Inorder Traversal.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ * time: O(n)
+ * space: O(n)
+ * 从前序遍历中获取根节点，从中序遍历中获取左右子树的起始结束下标，递归
+ */
+var buildTree = function(preorder, inorder) {
+  let getRoot = function(rootIndexPre, start, end) {
+    if (end < start) return null;
+    let root = preorder[rootIndexPre];
+    if (start === end) return new TreeNode(root);
+    let rootIndexIn = inorder.indexOf(root);
+    let leftLength = rootIndexIn - start;
+    return new TreeNode(root, getRoot(rootIndexPre + 1, start, rootIndexIn - 1), getRoot(rootIndexPre + leftLength + 1, rootIndexIn + 1, end))
+  };
+
+  return getRoot(0, 0, preorder.length - 1);
+};
