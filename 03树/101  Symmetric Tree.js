a7bfab40a756cdb8c03942cbd77a9c891257e01d
Lynn Yang <yangll2525@163.com>
2022-04-14 22:27:58 +0800

    101

---
+++ 03树/101  Symmetric Tree.js
@@ -0,0 +1,16 @@
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ * time: O(n)
+ * space: O(n)
+ * 树围绕中心对称，说明left.left = right.right; left.right = right.left;
+ * 递归比较节点的左右节点即可
+ */
+var isSymmetric = function(root) {
+  const compare = function(left, right) {
+    if (!left && !right) return true;
+    if (!left || !right) return false;
+    return left.val === right.val && compare(left.left, right.right) && compare(left.right, right.left);
+  };
+  return compare(root.left, root.right);
+};
