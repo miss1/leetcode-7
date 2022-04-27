5963ee535e3ea949faedfe49026a3a2903cd6a52
lynn <yangll2525@163.com>
2022-04-27 19:03:28 +0800

    35

--- 05双指针/35 Search Insert Position.js
+++ 05双指针/35 Search Insert Position.js
@@ -4,19 +4,16 @@
  * @return {number}
  * 从有序的数列中查找某个数的位置
  * 二分查找，两个指针分别指向头和尾，取中间的节点center跟target比较大小
- * 若 center > target,则尾部的指针移动到center。center < target,则头部指针移动到center。若相等则center就是所求位置
+ * 若 center > target,则尾部的指针移动到center-1。center < target,则头部指针移动到center+1。若相等则center就是所求位置
  * time: O(logn)
  * space: O(1)
  */
 var searchInsert = function(nums, target) {
-  if (nums[0] >= target) return 0;
-  if (nums[nums.length - 1] < target) return nums.length;
   let left = 0, right = nums.length - 1;
-  while (left < right) {
-    let center = left + Math.floor((right - left + 1) / 2);
-    if (center === left || center === right) return center;
-    if (nums[center] > target) right = center;
-    else if (nums[center] < target) left = center;
+  while (left <= right) {
+    let center = Math.floor((right + left) / 2);
+    if (nums[center] > target) right = center - 1;
+    else if (nums[center] < target) left = center + 1;
     else return center;
   }
   return left;
