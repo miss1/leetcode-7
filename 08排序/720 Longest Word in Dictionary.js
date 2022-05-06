2147ca4047ac6a04516bee7458893c84f412f909
lynn <yangll2525@163.com>
2022-05-06 23:35:21 +0800

    720

--- 08排序/720 Longest Word in Dictionary.js
+++ 08排序/720 Longest Word in Dictionary.js
@@ -27,3 +27,27 @@ var longestWord = function(words) {
   }
   return res;
 };
+
+
+/**
+ * @param {string[]} words
+ * @return {string}
+ * 比较巧妙的一个方法。先将words按字典序排序，然后遍历words
+ * 当当前字符word长度为1或者word去掉最后一个字符在set中存在时，说明当前word是一个解。存入set中，并且更新最优解
+ * 注：因为words已经排过序了，所以如果word是一个解，那么它前面的字符肯定已经在set里了
+ * eg：[a, ap, app, appl, apply]; a进入set，ap: a在set中说明ap是一个解，存到set. app: ap 在set中....
+ * time: O(n), n为words长度
+ * space: O(n)
+ */
+var longestWord2 = function(words) {
+  let res = '', set = new Set();
+  words.sort();
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length === 1 || set.has(words[i].slice(0, -1))) {
+      set.add(words[i]);
+      if (words[i].length > res.length) res = words[i];
+      else if (words[i].length === res.length) res = words[i] < res ? words[i] : res;
+    }
+  }
+  return res;
+};
