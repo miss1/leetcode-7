9657eecb5547187522d824a09ecf127d6ddcd489
Lynn Yang <yangll2525@163.com>
2022-05-09 23:56:24 +0800

    240

--- 13二分/240 Search a 2D Matrix II.js
+++ 13二分/240 Search a 2D Matrix II.js
@@ -4,7 +4,7 @@
  * @return {boolean}
  * 从矩阵左下角开始搜索，根据题意，最左下角的点(x,y)，同一y轴上的点都在它上面，都比它小；同一x轴上的点都在它右边，都比它大。
  * (x,y)处的点跟target相比，若相等，则返回true，若大于target，则x-1向上走一步（值减小）；如果小于target，则y+1向右走一步（值增大）
- * time: O(m*n)
+ * time: O(m+n)
  * space: O(1)
  */
 var searchMatrix = function(matrix, target) {
