1c5d19163fef84eb11cee7ca3902c683edaf7baf
lynn <yangll2525@163.com>
2022-05-20 22:47:20 +0800

    695

---
+++ 14搜索/695 Max Area of Island.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ * DFS, 跟200题类似，dfs中加一个返回值，返回每个小岛(本身)以及它四个方向小岛的总和。
+ * 因为已经把遍历过的1置为0，所以不会出现重复计算的情况
+ * time: O(m * n)
+ * space: O(m * n)
+ */
+var maxAreaOfIsland = function(grid) {
+  const dfs = function(i, j) {
+    if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] === 0) return 0;
+    grid[i][j] = 0;
+    return 1 + dfs(i + 1, j) + dfs(i - 1, j) + dfs(i, j + 1) + dfs(i, j - 1);
+  };
+  let res = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === 1) {
+        res = Math.max(res, dfs(i, j));
+      }
+    }
+  }
+  return res;
+};
