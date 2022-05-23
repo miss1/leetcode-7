a405a1c6801c1d72fab9ea9ab9e104fd4c6e378b
lynn <yangll2525@163.com>
2022-05-23 22:22:11 +0800

    39

---
+++ 14搜索/39 Combination Sum.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ * 回溯。先排序是为了去掉重复的答案
+ * time: O(n) n: 答案的总长度
+ * space: O(target) 递归栈的深度
+ */
+var combinationSum = function(candidates, target) {
+  let res = [];
+  candidates.sort((a,b) => a - b);
+  const backTrack = function(result, sum) {
+    if (sum > target) return;
+    if (sum === target) {
+      res.push([...result]);
+      return;
+    }
+    for (let i = 0; i < candidates.length; i++) {
+      if (result.length > 0 && candidates[i] < result[result.length - 1]) continue;
+      result.push(candidates[i]);
+      sum += candidates[i];
+      backTrack(result, sum);
+      let n = result.pop();
+      sum -= n;
+    }
+  };
+  backTrack([], 0);
+  return res;
+};
