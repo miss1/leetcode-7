13c568dca1e914db57c7a75f8484aac1ad632355
lynn <yangll2525@163.com>
2022-06-23 17:41:10 +0800

    28

--- 12滑动窗口/28 Implement strStr().js
+++ 12滑动窗口/28 Implement strStr().js
@@ -22,3 +22,32 @@ var strStr = function(haystack, needle) {
   }
   return s === needle ? start : -1;
 };
+
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ * KMP 方法
+ * time: O(n + m)
+ * space: O(m)
+ */
+var strStr2 = function(haystack, needle) {
+  if (needle.length === 0) return 0;
+  const n = haystack.length, m = needle.length;
+  const s = ' ' + haystack;
+  const p = ' ' + needle;
+  const next = new Array(m + 1).fill(0);
+  for (let i = 2, j = 0; i <= m; i++) {
+    while (j > 0 && p[i] !== p[j + 1]) j = next[j];
+    if (p[i] === p[j + 1]) j++;
+    next[i] = j;
+  }
+
+  for (let i = 1, j = 0; i <= n; i++) {
+    while (j > 0 && s[i] !== p[j + 1]) j = next[j];
+    if (s[i] === p[j + 1]) j++;
+    if (j === m) return i - m;
+  }
+  return -1;
+};
