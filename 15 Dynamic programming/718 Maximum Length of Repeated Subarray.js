4e8fcd33fe75e00dd26ede293f66c54aeebfe8a3
liling yang <yangll2525@163.com>
2022-12-11 20:15:12 -0800

    718

---
+++ 15 Dynamic programming/718 Maximum Length of Repeated Subarray.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ * dp[i][j] = dp[i+1][j+1] + 1
+ * 以i, j为结尾的子串，如果nums1[i] == nums2[j]，dp[i][j] = 1 + 以前一个字符结尾时的结果
+ * 新建一个二维数组arr，存储，当nums1中(0, i)以i下标结尾的子串，nums2中以j下标结尾的子串，他们的Maximum Length of Repeated Subarray
+ * 则 当nums1[i] == nums2[j]时，arr[i][j] = arr[i - 1][j - 1] + 1
+ * eg.nums1 = [1,2,3,2,1] nums2 = [3,2,1,4,7]
+ *    3  2  1  4  7
+ * 1  0  0  1  0  0
+ * 2  0  1  0  0  0
+ * 3  1  0  0  0  0
+ * 2  0  2  0  0  0
+ * 1  0  0  3  0  0
+ * so the Maximum Length of Repeated Subarray is 3
+ * time: O(mn)
+ * space: O(mn)
+ */
+var findLength = function(nums1, nums2) {
+  let arr = new Array(nums1.length).fill(0).map(() => new Array(nums2.length).fill(0));
+  let res = 0;
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums2.length; j++) {
+      if (nums1[i] === nums2[j]) {
+        arr[i][j] = i > 0 && j > 0 ? arr[i - 1][j - 1] + 1 : 1;
+        res = Math.max(res, arr[i][j]);
+      }
+    }
+  }
+  return res;
+};
