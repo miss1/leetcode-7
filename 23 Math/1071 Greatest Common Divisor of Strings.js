594e19c6800076eedb69fce61930e370790746ef
liling yang <yangll2525@163.com>
2022-12-28 15:37:43 -0800

    1071

---
+++ 23 Math/1071 Greatest Common Divisor of Strings.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ * 欧几里得算法（辗转相除法）求最大公约数。gcd(a,b) = gcd(b,a mod b)
+ * 求得字符串长度的最大公约数之后，截取对应的长度返回即可
+ * time: O(n)
+ * space: O(1)
+ */
+var gcdOfStrings = function(str1, str2) {
+  if (str1 + str2 !== str2 + str1) return '';
+  let a = str1.length, b = str2.length;
+  while (b !== 0) {
+    let t = b;
+    b = a % b;
+    a = t;
+  }
+  return str1.slice(0, a);
+};
+
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings2 = function(str1, str2) {
+  const gcd = function(a, b) {
+    if (b === 0) return a;
+    else return gcd(b, a % b);
+  };
+  if (str1 + str2 !== str2 + str1) return '';
+  let size = gcd(str1.length, str2.length);
+  return str1.slice(0, size);
+};
