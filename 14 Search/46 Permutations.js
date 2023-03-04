f8fd6033aebf33f6d75d9f3acc2c6573a8cbb90a
liling yang <yangll2525@163.com>
2023-03-03 23:38:24 -0800

    46, 78

--- 14 Search/46 Permutations.js
+++ 14 Search/46 Permutations.js
@@ -23,3 +23,30 @@ var permute = function(nums) {
   backTrack([]);
   return res;
 };
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ * 用hashmap存储已经记录的数字，取代result.includes
+ */
+var permute2 = function(nums) {
+  let res = [];
+  let set = new Set();
+  const backTrack = function(arr) {
+    if (arr.length === nums.length) {
+      res.push([...arr]);
+      return;
+    }
+    for (let i = 0; i < nums.length; i++) {
+      if (set.has(nums[i])) continue;
+      set.add(nums[i]);
+      arr.push(nums[i]);
+      backTrack(arr);
+      const n = arr.pop();
+      set.delete(n);
+    }
+  };
+  backTrack([]);
+  return res;
+};
--- 14 Search/78 Subsets.js
+++ 14 Search/78 Subsets.js
@@ -3,7 +3,7 @@
  * @return {number[][]}
  * 子集
  * 跟排列组合一个道理，只是每次回溯的时候将当前的result数组存储到res中作为一个返回结果
- * time: O(n), n: 结果的总长度
+ * time: O(n * 2^n), n: 结果的总长度
  * space: O(n)
  */
 var subsets = function(nums) {
@@ -19,4 +19,30 @@ var subsets = function(nums) {
   };
   backTrack([], 0);
   return res;
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ * 遍历nums，逐个将每一个num添加到result的每一个数组中
+ * eg: nums = [1,2,3]
+ * num = 1时， result = [[]] -> [].push(1), result = [[], [1]]
+ * num = 2时， result = [[], [1]] -> [] + 2, [1] + 2 -> [2], [1,2], 将结果添加到res中
+ * num = 3时， result = [[], [1], [2], [1,2]] -> [3], [1,3], [2,3], [1,2,3]
+ * time: O(n * Math.pow(2,n))
+ * space: (Math.pow(2,n))
+ */
+var subsets2 = function(nums) {
+  let result = [[]];
+  for (let num of nums) {
+    let newSubsets = [];
+    // 1 * 2 * 2...
+    for (let curr of result) {
+      newSubsets.push([...curr, num]);
+    }
+    for (let curr of newSubsets) {
+      result.push(curr);
+    }
+  }
+  return result;
 };
