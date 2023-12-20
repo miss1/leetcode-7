12595aeb69b663a928fc1a7889e14b0e984ff456
liling yang <yangll2525@163.com>
2023-12-19 17:53:04 -0800

    139

--- 15 Dynamic programming/139 Word Break.js
+++ 15 Dynamic programming/139 Word Break.js
@@ -21,3 +21,35 @@ var wordBreak = function(s, wordDict) {
   }
   return dp[s.length];
 };
+
+
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ * BFS
+ * s中每个index为一个node，如果i到j之间的字符存在于wordDict中，说明i到j存在一条边
+ * eg: s = "leetcode", wordDict = ["leet","code"], 则 0-3相连，4-7相连
+ * BFS求从0出发，能否到达7 -> [0] -> [4] -> [8] (到达下一个点j之后，下一个起点是j+1)
+ * time: O(n*n*n) n: s.length, m: wordDict.length, k: wordDict[i].length
+ */
+var wordBreak2 = function(s, wordDict) {
+  let set = new Set(wordDict);  // time: O(m), space: O(m*k)
+  let target = s.length;
+  let queue = [0], visited = new Set();  // time: O(n*n*n) space: O(n)
+  while (queue.length > 0) {
+    let nextLevel = [];
+    for (let n of queue) {
+      let w = '';
+      for (let i = n; i < target; i++) {
+        w += s[i];
+        if (visited.has(i) || !set.has(w)) continue;
+        if (i === target - 1) return true;
+        visited.add(i)
+        nextLevel.push(i + 1);
+      }
+    }
+    queue = nextLevel;
+  }
+  return false;
+};
