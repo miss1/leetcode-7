17ceb43fa9fc9f5d7bf6aad329fa2ae2848424bd
liling yang <yangll2525@163.com>
2024-01-01 13:26:14 -0800

    1624

---
+++ 04 Hash Map/1624 Largest Substring Between Two Equal Characters.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxLengthBetweenEqualCharacters = function(s) {
+  let arr = new Array(26).fill(-1);
+  let res = -1;
+  for (let i = 0; i < s.length; i++) {
+    const idx = s[i].charCodeAt(0) - 'a'.charCodeAt(0);
+    if (arr[idx] === -1) arr[idx] = i;
+    else res = Math.max(res, i - arr[idx] - 1);
+  }
+  return res;
+};
