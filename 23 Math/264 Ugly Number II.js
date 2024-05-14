b9f0a83834c8fba49f3294b83ba7962fe05c36cd
liling yang <yangll2525@163.com>
2024-05-13 17:29:50 -0700

    264

--- 23 Math/264 Ugly Number II.js
+++ 23 Math/264 Ugly Number II.js
@@ -3,7 +3,7 @@
  * @return {number}
  * 获取Ugly number: 从1开始，将目前获取的Ugly number分别乘以2，3，5来获取新的Ugly number
  * 用arr存储获取到的Ugly number(按顺序)
- * x,y,z从idx=0开始
+ * x,y,z从idx=0开始；每一个已知的number都需要乘以2，3，5。如果已经乘过了，则移动对应的idx(x,y,z)
  * 每次新增的Ugly number，arr[i] = Math.min(arr[x] * 2, arr[y] * 3, arr[z] * 5)
  * time: O(n)
  * space: O(n)
