2bf58f689472a9166a70d6225afc075e94dd49fc
liling yang <yangll2525@163.com>
2024-08-11 21:17:27 -0700

    703

--- 08 Sorting/703 Kth Largest Element in a Stream.js
+++ 08 Sorting/703 Kth Largest Element in a Stream.js
@@ -42,3 +42,42 @@ KthLargest.prototype.findIndex = function(val) {
  * var obj = new KthLargest(k, nums)
  * var param_1 = obj.add(val)
  */
+
+
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ * PriorityQueue
+ * 维持一个长度为k的最小pq，长度超过时删除堆顶（最小值）。这样pq里面的值永远是目前前k大的值
+ * O(n)
+ */
+var KthLargest2 = function(k, nums) {
+  const queue = new PriorityQueue({compare: (a, b) => a - b});
+  for (let n of nums) {
+    queue.enqueue(n);
+    if (queue.size() > k) {
+      queue.dequeue();
+    }
+  }
+  this.k = k;
+  this.queue = queue;
+};
+
+/**
+ * @param {number} val
+ * @return {number}
+ * O(1)
+ */
+KthLargest2.prototype.add = function(val) {
+  this.queue.enqueue(val);
+  if (this.queue.size() > this.k) {
+    this.queue.dequeue();
+  }
+  return this.queue.front();
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */
