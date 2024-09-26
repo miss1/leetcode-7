de4b49886e4db4496ff4c4fbbccb8f76990150bb
liling yang <yangll2525@163.com>
2024-09-26 10:38:22 -0700

    729

--- 13 Binary search/729 My Calendar I.js
+++ 13 Binary search/729 My Calendar I.js
@@ -34,3 +34,59 @@ MyCalendar.prototype.book = function(start, end) {
  * var obj = new MyCalendar()
  * var param_1 = obj.book(start,end)
  */
+
+
+
+
+var MyCalendar2 = function() {
+  this.arr = [];
+};
+
+/**
+ * @param {number} start
+ * @param {number} end
+ * @return {boolean}
+ * 两个stack
+ * 逐个比较[start,end]和arr中的每一个值，找到对应的位置插入
+ * time: O(n)
+ * space: O(n)
+ */
+MyCalendar2.prototype.book = function(start, end) {
+  if (this.arr.length === 0) {
+    this.arr.push([start, end]);
+    return true;
+  }
+
+  let tmp = [], tag = false;
+  while (this.arr.length > 0) {
+    const t = this.arr.pop();
+    if (start >= t[1]) {
+      this.arr.push(t);
+      this.arr.push([start, end]);
+      tag = true;
+      break;
+    }
+    if (end > t[0]) {
+      this.arr.push(t);
+      tag = false;
+      break;
+    }
+
+    tmp.push(t);
+    if (this.arr.length === 0) {
+      this.arr.push([start, end]);
+      tag = true;
+      break;
+    }
+  }
+  while (tmp.length > 0) {
+    this.arr.push(tmp.pop());
+  }
+  return tag;
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * var obj = new MyCalendar()
+ * var param_1 = obj.book(start,end)
+ */
